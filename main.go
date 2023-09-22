64008c7a4adc52a0c10519a7921f480df525c7f2
AbdelRahman Adel AbdelFatttah <56457765+ad3ldev@users.noreply.github.com>
2023-09-22 20:38:09 +0300

    Update main.go

--- main.go
+++ main.go
@@ -143,9 +143,31 @@ func newMatrix(rows int, cols int) [][]bool {
 	}
 	return m
 }
+
+func nextGeneration(rows int, cols int, array [][]bool) [][]bool {
+	nextGen := newMatrix(rows, cols)
+	for i := 0; i < rows; i++ {
+		for j := 0; j < cols; j++ {
+			nextGen[i][j] = checkCell(i, j, array)
+		}
+	}
+	return nextGen
+}
+func printGeneration(rows int, cols int, array [][]bool) {
+	for i := 0; i < rows; i++ {
+		for j := 0; j < cols; j++ {
+			if array[i][j] {
+				fmt.Print("*")
+			} else {
+				fmt.Print("_")
+			}
+		}
+		fmt.Println()
+	}
+}
 func main() {
-	rows := 10
-	cols := 10
+	rows := 12
+	cols := 12
 	array := newMatrix(rows, cols)
 	array[0][1] = true
 	array[1][2] = true
@@ -153,27 +175,16 @@ func main() {
 	array[2][1] = true
 	array[2][2] = true
 
+	array[8][2] = true
+	array[8][3] = true
+	array[8][4] = true
 	array[8][7] = true
 	array[8][8] = true
 	array[8][9] = true
+
 	for {
-		nextGen := newMatrix(rows, cols)
-		for i := 0; i < rows; i++ {
-			for j := 0; j < cols; j++ {
-				if array[i][j] {
-					fmt.Print("*")
-				} else {
-					fmt.Print("_")
-				}
-			}
-			fmt.Println()
-		}
-		for i := 0; i < rows; i++ {
-			for j := 0; j < cols; j++ {
-				nextGen[i][j] = checkCell(i, j, array)
-			}
-		}
-		copy(array, nextGen)
+		printGeneration(rows, cols, array)
+		array = nextGeneration(rows, cols, array)
 		time.Sleep(500 * time.Millisecond)
 		c := exec.Command("clear")
 		c.Stdout = os.Stdout
